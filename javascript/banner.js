55c80a7d72fe9bfcb78bc948fc8aac8c456a83ac
leenawon <nawon5154@gmail.com>
2021-01-31 20:28:02 +0900

    banner

---
+++ javascript/banner.js
@@ -0,0 +1,9 @@
+'use strict';
+const togleBtn = document.querySelector('.navbar_toogleBtn');
+const menu = document.querySelector('.navbar_menu');
+const icons = document.querySelector('.navbar_icons');
+
+togleBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  icons.classList.toggle('active');
+});
